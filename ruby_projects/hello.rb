25a539aa26bc04281a7e7b97484d2b8ed96f5281
MasakoIdei <tarte.groseilles@gmail.com>
2021-06-14 00:40:32 +0000

    [add]コメント機能について

--- ruby_projects/hello.rb
+++ ruby_projects/hello.rb
@@ -1,2 +1,11 @@
+#一行コメントの場合は、#を使用
+# puts 'Hello World!'
+# puts 10+3
+
+#複数行コメントの場合は、　=begin =end でコメントアウトしたい内容を挟む
+#ただし、=begin =endは余り使わないらしい………
+=begin
 puts 'Hello World!'
-puts 10+3
+puts 'Hello World!'
+puts 'Hello World!'
+=end
