757051627f67329214970b5afd239ccc801e7b66
MasakoIdei <tarte.groseilles@gmail.com>
2021-08-14 15:37:49 +0000

    [add]todoリスト続き

--- rails_projects/todo/app/controllers/tasks_controller.rb
+++ rails_projects/todo/app/controllers/tasks_controller.rb
@@ -3,9 +3,12 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+
   def new
     @task = Task.new
   end
+
   def create
     @task = Task.new(task_params)
      if @task.save
@@ -15,8 +18,31 @@ class TasksController < ApplicationController
      end
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    if @task.destroy
+      redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
 
   private
+
   def task_params
     params[:task].permit(:title)
   end
